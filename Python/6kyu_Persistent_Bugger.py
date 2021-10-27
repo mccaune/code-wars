9f285cbb2ee0929527733c00cbe3b31ec13aeb7e
mccaune <mairis.caune@gmail.com>
2021-10-27 18:34:48 +0300

    update

---
+++ Python/6kyu_Persistent_Bugger.py
@@ -0,0 +1,24 @@
+"""
+Write a function, persistence, that takes in a positive parameter num and returns its multiplicative persistence, which is the number of times you must multiply the digits in num until you reach a single digit.
+
+For example:
+
+ persistence(39) # returns 3, because 3*9=27, 2*7=14, 1*4=4
+                 # and 4 has only one digit
+                  
+ persistence(999) # returns 4, because 9*9*9=729, 7*2*9=126,
+                  # 1*2*6=12, and finally 1*2=2
+
+ persistence(4) # returns 0, because 4 is already a one-digit number
+"""
+
+def persistence(n):
+    sum = 1
+    times = 0
+    while n > 9:
+        for i in str(n):
+            sum = sum * int(i)
+        times += 1
+        n = sum
+        sum = 1
+    return times
